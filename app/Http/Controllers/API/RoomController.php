34deb5a15a2f3be908a7c40904bca1ae9ce35261
johnfrancislor <jf.lor@mlgcl.edu.ph>
2026-01-19 23:06:09 +0800

    Fix Forms

--- app/Http/Controllers/API/RoomController.php
+++ app/Http/Controllers/API/RoomController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Rooms extends Controller
+class RoomController extends Controller
 {
     //
 }
