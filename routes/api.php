eafa30258ddd662da49c8d92a09c697af7a4a812
Tyndarius1 <kb.dacera@mlgcl.edu.ph>
2026-01-21 00:08:57 +0800

    feat: Add AWS S3 support and enhance booking management
    - Updated composer.json to include league/flysystem-aws-s3-v3 for AWS S3 integration.
    - Modified filesystems.php to set default visibility for S3 storage.
    - Enhanced guests and bookings migration files to include additional fields and constraints.
    - Created BookingResource and associated pages (Create, Edit, List) for managing bookings in Filament.
    - Created GuestResource and associated pages (Create, Edit, List) for managing guests in Filament.
    - Implemented booking form schema with guest and room/venue selection.
    - Added table configuration for displaying bookings and guests with relevant filters and actions.
    - Configured Livewire settings for improved component management.

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\BookingController;
 use Illuminate\App\Http\Controllers\VenueController;
 use Illuminate\App\Http\Controllers\RoomsController;
-use Illuminate\App\Http\Controllers\BookingsController;
 use Illuminate\App\Http\Controllers\ImagesController;
 use Illuminate\App\Http\Controllers\GuestController;
 
@@ -13,3 +13,8 @@ use Illuminate\App\Http\Controllers\GuestController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+// Guests can create a booking
+Route::post('/bookings', [BookingController::class, 'store']);
