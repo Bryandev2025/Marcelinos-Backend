2562097666b1827760efac20c735f50f4c8be4a6
kylzlviii <236215584+kylzlviii@users.noreply.github.com>
2026-01-20 22:58:47 +0800

    add guest and bookings API

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -2,13 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Booking extends Model
 {
-    protected $guarded = [];
+     use HasFactory;
 
-    public function guest() { return $this->belongsTo(Guest::class); }
-    public function room() { return $this->belongsTo(Room::class); }
-    public function venue() { return $this->belongsTo(Venue::class); }
+    protected $fillable = [
+    'guest_id',
+    'room_id',
+    'reference_id',
+    'check_in',
+    'check_out',
+    'total_price',
+    'status',
+];
+
+    public function guest()
+    {
+        return $this->belongsTo(Guest::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
