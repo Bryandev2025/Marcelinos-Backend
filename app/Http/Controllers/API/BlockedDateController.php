ab5d56da7c2491b6dbb4145af0eb81706ed8aa9d
kritzpl <christinalumiguid@gmail.com>
2026-02-09 21:42:13 +0800

    modify api response

--- app/Http/Controllers/API/BlockedDateController.php
+++ app/Http/Controllers/API/BlockedDateController.php
@@ -11,16 +11,14 @@ class BlockedDateController extends Controller
 {
     use CachesApiResponses;
 
-    /** Cache TTL for blocked dates list (10 min). */
-    protected static int $defaultCacheTtl = 600;
-
     /**
      * Return all blocked dates as JSON (cached for performance).
      */
     public function index(): JsonResponse
     {
         return $this->rememberJson('api.blocked-dates', function () {
-            $blockedDates = BlockedDate::pluck('date');
+            $blockedDates = BlockedDate::select('date', 'reason')->get();
+
             return response()->json(['blocked_dates' => $blockedDates]);
         });
     }
