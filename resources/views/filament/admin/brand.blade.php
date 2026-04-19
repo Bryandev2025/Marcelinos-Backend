155ec5f5af8950f7b23f5065d4e622d0b602722d
kylzl <liezlkayeroa72317@gmail.com>
2026-04-19 11:40:03 +0800

    fix: correct brand name from "RESORT AND HOTEL" to "RESORT HOTEL"

--- resources/views/filament/admin/brand.blade.php
+++ resources/views/filament/admin/brand.blade.php
@@ -8,21 +8,21 @@
     <img
         src="{{ asset('brand-logo.webp') }}"
         alt="Marcelino's Logo"
-        style="height: 48px; width: auto;"
+        style="height: 45px; width: auto;"
         loading="lazy"
     >
+        <div class="ml-1 leading-tight w-fit text-shadow-[0_0.5px_1px_rgba(0.9,0.9,0.9,0.9)] ">
+            <div 
+            class="text-sm font-serif tracking-[0.13rem] whitespace-nowrap text-center"
+            :class="isDark ? 'text-[#fff]' : 'text-[#456e26]'">
+                MARCELINO'S
+            </div>
 
-    <div class="ml-2 leading-tight">
-        <div 
-          class="text-[16px] font-extrabold tracking-widest font-serif"
-          :class="isDark ? 'text-[#09DF72]' : 'text-[#044835]'">
-            MARCELINO'S
+            <div 
+            class="text-xs tracking-[0.2rem] text-center"
+            :class="isDark ? 'text-[#E6D3A3]' : 'text-[#E6D3A3]'">
+                RESORT HOTEL
+            </div>
         </div>
-        <div 
-          class="text-xs font-medium tracking-widest"
-          :class="isDark ? 'text-gray-300' : 'text-gray-900'">
-            RESORT AND HOTEL
-        </div>
-    </div>
 
 </div>
